f2ed60873e369987d403dccb38f6699a1391bb88
Nicce <niclaslindgren91@hotmail.com>
2015-11-02 10:46:03 +0200

    Cube replaced with ship.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 var THREE = require('three');
 var THREEx = {
-  KeyboardState: require('./threex.keyboardstate'),
-  ObjectLoader: require('./OBJMTLLoader')
+  KeyboardState: require('./threex.keyboardstate')
 };
 
 var scene = new THREE.Scene();
@@ -11,48 +10,39 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial();
 var ambientLight = new THREE.AmbientLight(0x222222, 0.1);
 var light = new THREE.DirectionalLight(0xffffff, 1);
-var cube = new THREE.Mesh( geometry, material );
 var keyboard	= new THREEx.KeyboardState(renderer.domElement);
 renderer.domElement.setAttribute("tabIndex", "0");
 renderer.domElement.focus();
-cube.rotation.x += 0.4;
-scene.add(cube);
 scene.add(ambientLight);
 scene.add(light);
 camera.position.z = 5;
 
 // prepare loader and load the model
-THREEx.ObjectLoader().load('media/SpaceFighter01/SpaceFighter01.obj',
-  'media/SpaceFighter01/SpaceFighter01.mtl', function(object) {
-
-  object.position.x = 0;
-  object.position.y = 0;
-  object.position.z = 0;
-  object.scale.set(0.1, 0.1, 0.1);
-  scene.add(object);
-});
-
+var loader = new THREE.ObjectLoader();
+var ship;
+loader.load( './media/star-wars-vader-tie-fighter.json', function ( object ) {
+  ship = object;
+  scene.add( ship );
+} );
 
 var render = function () {
   requestAnimationFrame( render );
   var delta = 0.2;
   if( keyboard.pressed('left') ){
-    cube.rotation.y -= 1 * delta;
-    cube.position.x -= 1 * delta;
+    ship.rotation.y -= 1 * delta;
+    ship.position.x -= 1 * delta;
   }else if( keyboard.pressed('right') ){
-    cube.rotation.y += 1 * delta;
-    cube.position.x += 1 * delta;
+    ship.rotation.y += 1 * delta;
+    ship.position.x += 1 * delta;
   }
   if( keyboard.pressed('down') ){
-    cube.rotation.x += 1 * delta;
-    cube.position.y -= 1 * delta;
+    ship.rotation.x += 1 * delta;
+    ship.position.y -= 1 * delta;
   }else if( keyboard.pressed('up') ){
-    cube.rotation.x -= 1 * delta;
-    cube.position.y += 1 * delta;
+    ship.rotation.x -= 1 * delta;
+    ship.position.y += 1 * delta;
   }
   renderer.render( scene, camera );
 };
