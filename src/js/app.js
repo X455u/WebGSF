6023807e418aedcc360896d8bb0902abb05d6b27
Kasper Hellström <kassu7@gmail.com>
2015-12-22 18:06:18 +0200

    Added ESLint and fixed linting errors.

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,8 @@ document.body.appendChild(renderer.domElement);
 
 var ambientLight = new THREE.AmbientLight(0x222222, 0.1);
 var light = new THREE.DirectionalLight(0xffffff, 1);
-var keyboard	= new THREEx.KeyboardState(renderer.domElement);
-renderer.domElement.setAttribute("tabIndex", "0");
+var keyboard = new THREEx.KeyboardState(renderer.domElement);
+renderer.domElement.setAttribute('tabIndex', '0');
 renderer.domElement.focus();
 
 scene.add(ambientLight);
@@ -24,25 +24,25 @@ camera.position.z = 5;
 var loader = new THREE.ObjectLoader();
 var ship;
 var fakeShip = new THREE.Object3D();
-loader.load( './media/star-wars-vader-tie-fighter.json', function ( object ) {
+loader.load('./media/star-wars-vader-tie-fighter.json', function(object) {
   ship = object;
-  scene.add( ship );
+  scene.add(ship);
   // Steer smoothing helpers
   fakeShip.rotation.set(ship.rotation.x, ship.rotation.y, ship.rotation.z);
   ship.velocity = 0;
   ship.maxVelocity = 4;
   ship.acceleration = 2;
-} );
+});
 
 // Format debugging text
 var text = document.createElement('div');
 text.style.position = 'absolute';
 text.style.width = 100;
 text.style.height = 100;
-text.style.color = "white";
+text.style.color = 'white';
 text.style.top = 10 + 'px';
 text.style.left = 10 + 'px';
-text.innerHTML = "Loading..."
+text.innerHTML = 'Loading...';
 document.body.appendChild(text);
 
 // Terrain testing
@@ -50,37 +50,37 @@ var mapX = 200;
 var mapY = mapX; // Does not yet work with different x y dimensions
 var heightMap = new Array(mapX);
 for (var i = 0; i < mapX; i++) {
-	heightMap[i] = new Array(mapY);
+  heightMap[i] = new Array(mapY);
 }
 var map = new THREE.Geometry();
 
-for (x = 0; x < mapX; x++) {
-	for (y = 0; y < mapY; y++) {
-		heightMap[x][y] = Math.floor((Math.random() * 8));
-		map.vertices.push( new THREE.Vector3( x, y, heightMap[x][y] ) );
-	}
+for (var x = 0; x < mapX; x++) {
+  for (var y = 0; y < mapY; y++) {
+    heightMap[x][y] = Math.floor((Math.random() * 8));
+    map.vertices.push(new THREE.Vector3(x, y, heightMap[x][y]));
+  }
 }
 
-for (var x = 0; x < mapX-1; x++) {
-	for (var y = 0; y < mapY-1; y++) {
-		map.faces.push( new THREE.Face3( x*mapY + y+1, x*mapY + y, (x+1)*mapX + y ));
-		map.faces.push( new THREE.Face3( (x+1)*mapY + y+1, x*mapY + y+1, (x+1)*mapX + y ));
-	}
+for (x = 0; x < mapX - 1; x++) {
+  for (y = 0; y < mapY - 1; y++) {
+    map.faces.push(new THREE.Face3(x * mapY + y + 1, x * mapY + y, (x + 1) * mapX + y));
+    map.faces.push(new THREE.Face3((x + 1) * mapY + y + 1, x * mapY + y + 1, (x + 1) * mapX + y));
+  }
 }
 map.computeFaceNormals();
 
-var mapReady = new THREE.Mesh( map, new THREE.MeshNormalMaterial() );
+var mapReady = new THREE.Mesh(map, new THREE.MeshNormalMaterial());
 scene.add(mapReady);
-mapReady.rotation.x = -0.8
+mapReady.rotation.x = -0.8;
 mapReady.position.x = -10;
 mapReady.position.y = -10;
 mapReady.position.z = -10;
 
 // Ship thrust
 var shipThrust = false;
-keyboard.domElement.addEventListener('keydown', function(event){
+keyboard.domElement.addEventListener('keydown', function(event) {
   if (event.repeat) {return;}
-  if ( keyboard.eventMatches(event, 'space') ){
+  if (keyboard.eventMatches(event, 'space')) {
     shipThrust = !shipThrust;
   }
 });
@@ -89,32 +89,31 @@ keyboard.domElement.addEventListener('keydown', function(event){
 var fakeCam = new THREE.Object3D();
 
 // Game Loop
-var render = function () {
-  requestAnimationFrame( render );
+var render = function() {
+  requestAnimationFrame(render);
   var delta = 0.1;
 
   // Ship steering
-  if( keyboard.pressed('left') ){
-    fakeShip.rotateOnAxis(new THREE.Vector3( 0, 0, 1 ), delta);
-  }else if( keyboard.pressed('right') ){
-    fakeShip.rotateOnAxis(new THREE.Vector3( 0, 0, 1 ), -delta);
+  if (keyboard.pressed('left')) {
+    fakeShip.rotateOnAxis(new THREE.Vector3(0, 0, 1), delta);
+  } else if (keyboard.pressed('right')) {
+    fakeShip.rotateOnAxis(new THREE.Vector3(0, 0, 1), -delta);
   }
-  if( keyboard.pressed('down') ){
-    fakeShip.rotateOnAxis(new THREE.Vector3( 1, 0, 0 ), delta);
-  }else if( keyboard.pressed('up') ){
-    fakeShip.rotateOnAxis(new THREE.Vector3( 1, 0, 0 ), -delta);
+  if (keyboard.pressed('down')) {
+    fakeShip.rotateOnAxis(new THREE.Vector3(1, 0, 0), delta);
+  } else if (keyboard.pressed('up')) {
+    fakeShip.rotateOnAxis(new THREE.Vector3(1, 0, 0), -delta);
   }
-  ship.quaternion.slerp( fakeShip.quaternion, 0.1 );
+  ship.quaternion.slerp(fakeShip.quaternion, 0.1);
 
   // Ship move
   if (shipThrust) {
-    ship.velocity = Math.min( ship.maxVelocity,
-      ship.velocity + ship.acceleration * delta );
+    ship.velocity = Math.min(ship.maxVelocity,
+      ship.velocity + ship.acceleration * delta);
   } else {
-    ship.velocity = Math.max( 0,
-      ship.velocity - ship.acceleration * delta )
-  };
-  ship.translateZ( -ship.velocity * delta );
+    ship.velocity = Math.max(0, ship.velocity - ship.acceleration * delta);
+  }
+  ship.translateZ(-ship.velocity * delta);
 
   // Camera follow
   fakeCam.position.set(ship.position.x, ship.position.y, ship.position.z);
@@ -123,12 +122,12 @@ var render = function () {
   camera.position.lerp(fakeCam.position, 0.1);
   camera.quaternion.slerp(ship.quaternion, 0.1);
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 
-	//Update debugging text
-	text.innerHTML = "X: " + ship.position.x +
-		"<br/>" + "Y: " + ship.position.y +
-		"<br/>" + "Z: " + ship.position.z;
+  //Update debugging text
+  text.innerHTML = 'X: ' + ship.position.x +
+    '<br/>Y: ' + ship.position.y +
+    '<br/>Z: ' + ship.position.z;
 };
 
 render();
