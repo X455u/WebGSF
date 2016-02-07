4f22d0bfb3d904d2adc663f4ac9751ce1cefc323
Nicce <niclaslindgren91@hotmail.com>
2016-02-07 13:04:05 +0200

    ParticleEmitter is now an own class

---
+++ src/js/ParticleEmitter.js
@@ -0,0 +1,113 @@
+import THREE from 'three';
+
+function applyPointRandomness(r) {
+  let p = new THREE.Vector3(
+    (Math.random() - 0.5),
+    (Math.random() - 0.5),
+    (Math.random() - 0.5))
+    .normalize()
+    .multiplyScalar(r);
+  return p;
+}
+
+function newParticle(oldPosition, newPosition, lerpFactor, offset, pointRandomness) {
+  let result = new THREE.Vector3();
+  result.copy(oldPosition);
+  result.lerp(newPosition.clone().add(offset), lerpFactor);
+  result.add(applyPointRandomness(pointRandomness));
+  return result;
+}
+
+function calculateOldPosition(oldPosition, newPosition, offset) {
+  let result = new THREE.Vector3();
+  result.copy(oldPosition);
+  result.lerp(newPosition.clone().add(offset), 1);
+  return result;
+}
+
+class ParticleEmitter extends THREE.Points {
+
+  constructor(options) {
+    super();
+
+    this.offset = options.offset;
+    this.oldPosition = this.offset.clone();
+    this.bindTo = options.bindTo;
+    this.spawnRate = options.spawnRate;
+    this.lifetime = options.lifetime;
+    this.velocity = options.velocity;
+    this.velocityRandomness = options.velocityRandomness;
+    this.pointRandomness = options.pointRandomness;
+
+    this.geometry = new THREE.Geometry();
+    let toSpawn = Math.ceil(this.spawnRate * this.lifetime);
+    for (let i = 0; i < toSpawn; i++) {
+      let point = newParticle(
+        this.oldPosition,
+        this.bindTo.position,
+        1,
+        this.offset.clone().applyQuaternion(this.bindTo.quaternion),
+        this.pointRandomness
+      );
+      this.geometry.vertices[i] = point;
+      point.velocity = this.velocity.clone();
+      point.velocity.multiplyScalar(1 - this.velocityRandomness * (2 * Math.random() - 1));
+    }
+
+    this.material = new THREE.PointsMaterial({
+      color: options.color,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+      map: options.map,
+      size: options.size
+    });
+
+    this.iterator = 0;
+    this.geometry.computeBoundingSphere();
+  }
+
+  update(delta) {
+    // Helpers for lerping between frames
+    let n = 0;
+    let toSpawn = Math.ceil(this.spawnRate * delta + 1);
+    // Index of last particle (minus one) to update
+    let max = (this.iterator + toSpawn) % this.geometry.vertices.length;
+    // Reposition ("respawn") particles
+    while (this.iterator !== max) {
+      n++;
+      // New position
+      let point = newParticle(
+        this.oldPosition,
+        this.bindTo.position,
+        n / toSpawn,
+        this.offset.clone().applyQuaternion(this.bindTo.quaternion),
+        this.pointRandomness
+      );
+      this.geometry.vertices[this.iterator].copy(point);
+
+      // New velocity
+      point.velocity = this.velocity.clone();
+      point.velocity.multiplyScalar(1 - this.velocityRandomness * (2 * Math.random() - 1));
+      // Iterate to next particle
+      this.iterator = (this.iterator + 1) % this.geometry.vertices.length;
+    }
+    // Put position of last particle (without randomness) to oldPosition
+    this.oldPosition = calculateOldPosition(
+      this.oldPosition,
+      this.bindTo.position,
+      this.offset.clone().applyQuaternion(this.bindTo.quaternion)
+    );
+    // Update particles
+    this.geometry.vertices.forEach(particle => {
+      // Update velocity
+      let velocity = particle.velocity.clone();
+      velocity.applyQuaternion(this.bindTo.quaternion);
+      particle.addScaledVector(velocity, delta);
+    });
+    this.geometry.verticesNeedUpdate = true;
+    this.geometry.computeBoundingSphere();
+  }
+
+}
+
+export default ParticleEmitter;
--- src/js/ParticleSystem.js
+++ src/js/ParticleSystem.js
@@ -1,29 +1,4 @@
-import THREE from 'three';
-
-function applyPointRandomness(r) {
-  let p = new THREE.Vector3(
-    (Math.random() - 0.5),
-    (Math.random() - 0.5),
-    (Math.random() - 0.5))
-    .normalize()
-    .multiplyScalar(r);
-  return p;
-}
-
-function newParticle(oldPosition, newPosition, lerpFactor, offset, pointRandomness) {
-  let result = new THREE.Vector3();
-  result.copy(oldPosition);
-  result.lerp(newPosition.clone().add(offset), lerpFactor);
-  result.add(applyPointRandomness(pointRandomness));
-  return result;
-}
-
-function calculateOldPosition(oldPosition, newPosition, offset) {
-  let result = new THREE.Vector3();
-  result.copy(oldPosition);
-  result.lerp(newPosition.clone().add(offset), 1);
-  return result;
-}
+import ParticleEmitter from './ParticleEmitter';
 
 class ParticleSystem {
 
@@ -34,93 +9,14 @@ class ParticleSystem {
 
   update(delta) {
     this.emitters.forEach(emit => {
-      this.updateEmitter(emit, delta);
+      emit.update(delta);
     });
   }
 
   createEmitter(options) {
-    let emitter = new THREE.Points();
-
-    emitter.offset = options.offset;
-    emitter.oldPosition = emitter.offset.clone();
-    emitter.bindTo = options.bindTo;
-    emitter.spawnRate = options.spawnRate;
-    emitter.lifetime = options.lifetime;
-    emitter.velocity = options.velocity;
-    emitter.velocityRandomness = options.velocityRandomness;
-    emitter.pointRandomness = options.pointRandomness;
-
-    emitter.geometry = new THREE.Geometry();
-    let toSpawn = Math.ceil(emitter.spawnRate * emitter.lifetime);
-    for (let i = 0; i < toSpawn; i++) {
-      let point = newParticle(
-        emitter.oldPosition,
-        emitter.bindTo.position,
-        1,
-        emitter.offset.clone().applyQuaternion(emitter.bindTo.quaternion),
-        emitter.pointRandomness
-      );
-      emitter.geometry.vertices[i] = point;
-      point.velocity = emitter.velocity.clone();
-      point.velocity.multiplyScalar(1 - emitter.velocityRandomness * (2 * Math.random() - 1));
-    }
-
-    emitter.material = new THREE.PointsMaterial({
-      color: options.color,
-      blending: THREE.AdditiveBlending,
-      transparent: true,
-      map: options.map,
-      size: options.size
-    });
-
-    emitter.iterator = 0;
-    emitter.geometry.computeBoundingSphere();
-
-    this.emitters.push(emitter);
-    this.scene.add(emitter);
-    return emitter;
-  }
-
-  updateEmitter(emit, delta) {
-    // Helpers for lerping between frames
-    let n = 0;
-    let toSpawn = Math.ceil(emit.spawnRate * delta + 1);
-    // Index of last particle (minus one) to update
-    let max = (emit.iterator + toSpawn) % emit.geometry.vertices.length;
-    // Reposition ("respawn") particles
-    while (emit.iterator !== max) {
-      n++;
-      // New position
-      let point = newParticle(
-        emit.oldPosition,
-        emit.bindTo.position,
-        n / toSpawn,
-        emit.offset.clone().applyQuaternion(emit.bindTo.quaternion),
-        emit.pointRandomness
-      );
-      emit.geometry.vertices[emit.iterator].copy(point);
-
-      // New velocity
-      point.velocity = emit.velocity.clone();
-      point.velocity.multiplyScalar(1 - emit.velocityRandomness * (2 * Math.random() - 1));
-      // Iterate to next particle
-      emit.iterator = (emit.iterator + 1) % emit.geometry.vertices.length;
-    }
-    // Put position of last particle (without randomness) to oldPosition
-    emit.oldPosition = calculateOldPosition(
-      emit.oldPosition,
-      emit.bindTo.position,
-      emit.offset.clone().applyQuaternion(emit.bindTo.quaternion)
-    );
-    // Update particles
-    emit.geometry.vertices.forEach(particle => {
-      // Update velocity
-      let velocity = particle.velocity.clone();
-      velocity.applyQuaternion(emit.bindTo.quaternion);
-      particle.addScaledVector(velocity, delta);
-    });
-    emit.geometry.verticesNeedUpdate = true;
-    emit.geometry.computeBoundingSphere();
+    let newEmitter = new ParticleEmitter(options);
+    this.emitters.push(newEmitter);
+    this.scene.add(newEmitter);
   }
 
 }
