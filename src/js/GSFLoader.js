bda28813018e6364f98a6dc8fd3e0b85023d53fb
N1cc3 <niclaslindgren91@hotmail.com>
2017-09-05 00:30:32 +0300

    Loader class implemented. Loads and provides all game assets.

--- src/js/GSFLoader.js
+++ src/js/GSFLoader.js
@@ -1,12 +1,11 @@
 import THREE from 'three';
 
-const JSON_LOADER = new THREE.JSONLoader();
-const TEX_LOADER = new THREE.TextureLoader();
 
 class GSFLoader {
-  constructor(onStart, onProgress, onError, onLoad) {
+  constructor(onStart, onProgress, onError) {
     this.manager = new THREE.LoadingManager(onStart, onProgress, onError);
-    this.manager.onLoad = onLoad;
+    this.JSON_LOADER = new THREE.JSONLoader(this.manager);
+    this.TEX_LOADER = new THREE.TextureLoader(this.manager);
     this.assets = {};
   }
 
@@ -16,20 +15,20 @@ class GSFLoader {
 
   load() {
     /* eslint-disable dot-notation */
-    JSON_LOADER.load('./media/nicce_fighter.json', (geometry) => {
+    this.JSON_LOADER.load('./media/nicce_fighter.json', (geometry) => {
       geometry.scale(0.5, 0.5, 0.5);
       geometry.rotateY(Math.PI);
       this.assets['fighterGeometry'] = geometry;
     });
-    TEX_LOADER.load('./media/spaceship_comp.png', (texture) => {
-      TEX_LOADER.load('./media/spaceship_nor.png', (normalMap) => {
+    this.TEX_LOADER.load('./media/spaceship_comp.png', (texture) => {
+      this.TEX_LOADER.load('./media/spaceship_nor.png', (normalMap) => {
         this.assets['fighterMaterial'] = new THREE.MeshPhongMaterial({
           map: texture,
           normalMap: normalMap
         });
       });
     });
-    TEX_LOADER.load('./media/background.jpg', (texture) => {
+    this.TEX_LOADER.load('./media/background.jpg', (texture) => {
       this.assets['backgroundTexture'] = texture;
     });
     /* eslint-enable dot-notation */
--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ import EnemyShip from './EnemyShip';
 import HUD from './HUD';
 import Sun from './Sun';
 import {Howl} from 'howler';
-// import GSFLoader from './GSFLoader';
+import GSFLoader from './GSFLoader';
 
 const DEBUG = false;
 
@@ -21,135 +21,107 @@ const SPOTLIGHT_VECTOR = new THREE.Vector3(0, 0, 300);
 
 const MAX_DELTA = 0.1; // s
 
-
 // Loading
-// let i = 0;
-// let loadingText = document.createElement('div');
-// loadingText.className = 'loading';
-// loadingText.innerHTML = 'Loading...';
-// document.body.appendChild(loadingText);
-// let loadingProgress = document.createElement('div');
-// loadingProgress.style.display = 'flex';
-// loadingText.appendChild(loadingProgress);
-// let loadingProgressCount = document.createElement('div');
-// loadingProgress.innerHTML = i;
-// loadingProgress.appendChild(loadingProgressCount);
-// let loadingProgressTotal = document.createElement('div');
-// loadingProgressTotal.innerHTML = '/4';
-// loadingProgress.appendChild(loadingProgressTotal);
-//
-// let onStart = () => {
-//
-// };
-// let onProgress = () => {
-//   loadingProgress.innerHTML = ++i;
-// };
-// let onError = () => {
-//
-// };
-// let onLoad = () => {
-//
-// };
-// let loader = new GSFLoader(onStart, onProgress, onError, onLoad);
-// loader.load();
+let loadingText = document.createElement('div');
+loadingText.className = 'loading';
+loadingText.innerHTML = 'Loading...';
+document.body.appendChild(loadingText);
+let loadingProgress = document.createElement('div');
+loadingProgress.style.display = 'flex';
+loadingText.appendChild(loadingProgress);
+let loadingProgressCount = document.createElement('div');
+loadingProgress.appendChild(loadingProgressCount);
+let loadingProgressTotal = document.createElement('div');
+loadingProgress.appendChild(loadingProgressTotal);
 
-let music = new Howl({
-  src: ['./media/main_theme2.mp3'],
-  loop: true,
-  volume: 0.5
-});
-music.play();
+let onStart = (url, itemsLoaded, itemsTotal) => {
+  loadingProgressCount.innerHTML = itemsLoaded;
+  loadingProgressTotal.innerHTML = '/' + itemsTotal;
+};
+let onProgress = (url, itemsLoaded, itemsTotal) => {
+  loadingProgressCount.innerHTML = itemsLoaded;
+  loadingProgressTotal.innerHTML = '/' + itemsTotal;
+};
+let onError = (url) => {
+  console.error('Error loading: ' + url);
+};
+let loader = new GSFLoader(onStart, onProgress, onError);
 
-let scene = new THREE.Scene();
-let aspect = window.innerWidth / window.innerHeight;
-let camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000000);
-let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.autoClear = false;
-document.body.appendChild(renderer.domElement);
-renderer.domElement.setAttribute('tabIndex', '0');
-renderer.domElement.focus();
+function initGame() {
+  console.log('Loading complete!');
+  let music = new Howl({
+    src: ['./media/main_theme2.mp3'],
+    loop: true,
+    volume: 0.5
+  });
+  music.play();
 
-let ambientLight = new THREE.AmbientLight(0x222222, 0.1);
-let spotlight = new THREE.SpotLight(0xffffff, 3, 1000);
 
-scene.add(ambientLight);
-scene.add(spotlight);
-camera.position.z = -CAMERA_DISTANCE;
-camera.rotateOnAxis(camera.up, Math.PI);
+  let scene = new THREE.Scene();
+  let aspect = window.innerWidth / window.innerHeight;
+  let camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000000);
+  let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.autoClear = false;
+  document.body.appendChild(renderer.domElement);
+  renderer.domElement.setAttribute('tabIndex', '0');
+  renderer.domElement.focus();
+
+  let ambientLight = new THREE.AmbientLight(0x222222, 0.1);
+  let spotlight = new THREE.SpotLight(0xffffff, 3, 1000);
+
+  scene.add(ambientLight);
+  scene.add(spotlight);
+  camera.position.z = -CAMERA_DISTANCE;
+  camera.rotateOnAxis(camera.up, Math.PI);
+
+  let shotController = new ShotController(scene);
+  let particleSystem = new ParticleSystem(scene);
+
+
 
 // prepare loader and load the model
-let loader = new THREE.JSONLoader();
-let texLoader = new THREE.TextureLoader();
-let ship;
-let shipGeometry;
-let shipMaterial;
-let shotController = new ShotController(scene);
-let particleSystem = new ParticleSystem(scene);
-let crosshair;
-let loadPromise = new Promise(done => {
-  function next() {
-    texLoader.load('./media/background.jpg', (backgroundTexture) => {
-      let material = new THREE.MeshBasicMaterial({
-        map: backgroundTexture,
-        side: THREE.BackSide,
-        color: 0x555555
-      });
-      let geometry = new THREE.SphereGeometry(100000, 32, 32);
-      let stars = new THREE.Mesh(geometry, material);
-      scene.add(stars);
-      done();
-    });
-  }
-  texLoader.load('./media/spaceship_comp.png', function(texture) {
-    texLoader.load('./media/spaceship_nor.png', function(normalMap) {
-      loader.load('./media/nicce_fighter.json', function(geometry) {
-        let material = new THREE.MeshPhongMaterial({
-          map: texture,
-          normalMap: normalMap
-        });
-        geometry.scale(0.5, 0.5, 0.5);
-        geometry.rotateY(Math.PI);
-        let mesh = new THREE.Mesh(geometry, material);
-        shipGeometry = geometry;
-        shipMaterial = material;
-        ship = new Ship(mesh, shotController, particleSystem);
-        ship.isPlayer = true;
-        shotController.addHitbox(ship);
-        scene.add(ship);
-        crosshair = new Crosshair(scene, camera, ship);
-      });
-    });
-    next();
-  });
-});
-
+  let mesh = new THREE.Mesh(loader.get('fighterGeometry'), loader.get('fighterMaterial'));
+  let ship = new Ship(mesh, shotController, particleSystem);
+  ship.isPlayer = true;
+  shotController.addHitbox(ship);
+  scene.add(ship);
+  let crosshair = new Crosshair(scene, camera, ship);
 // Planet testing
-let planet = new Planet(500);
-planet.position.y = -800;
-scene.add(planet);
-shotController.addHitbox(planet);
+  let planet = new Planet(500);
+  planet.position.y = -800;
+  scene.add(planet);
+  shotController.addHitbox(planet);
 
 // Sun
-let sun = new Sun();
-sun.position.z = 10000;
-scene.add(sun);
+  let sun = new Sun();
+  sun.position.z = 10000;
+  scene.add(sun);
+
+// Background
+  let material = new THREE.MeshBasicMaterial({
+    map: loader.get('backgroundTexture'),
+    side: THREE.BackSide,
+    color: 0x555555
+  });
+  let geometry = new THREE.SphereGeometry(100000, 32, 32);
+  let stars = new THREE.Mesh(geometry, material);
+  scene.add(stars);
 
 // Format debugging text
-let text;
-let fps = 60.0;
-if (DEBUG) {
-  text = document.createElement('div');
-  text.className = 'debug';
-  text.innerHTML = 'Loading...';
-  document.body.appendChild(text);
-}
+  let text;
+  let fps = 60.0;
+  if (DEBUG) {
+    text = document.createElement('div');
+    text.className = 'debug';
+    text.innerHTML = 'Loading...';
+    document.body.appendChild(text);
+  }
 
 // Enemies
-let enemies = [];
-function initEnemies() {
+  let enemies = [];
   for (let i = 0; i < 5; i++) {
-    let enemy = new EnemyShip(shipGeometry, shipMaterial, shotController, particleSystem);
+    let enemy = new EnemyShip(loader.get('fighterGeometry'), loader.get('fighterMaterial'), shotController, particleSystem);
     let offset = new THREE.Vector3(Math.random(), Math.random(), Math.random());
     offset.multiplyScalar(i * 5);
     enemy.position.add(offset);
@@ -158,62 +130,64 @@ function initEnemies() {
     enemies.push(enemy);
     shotController.addHitbox(enemy);
   }
-}
 
 // HUD
-let hud = new HUD(window);
-hud.createBasicHUD();
+  let hud = new HUD(window);
+  hud.createBasicHUD();
 
 // Game Loop
-let previousTime;
-function render() {
-  let time = new Date().getTime();
-  let delta = Math.min(MAX_DELTA, (time - previousTime) / 1000);
-  previousTime = time;
+  let previousTime;
+  function render() {
+    let time = new Date().getTime();
+    let delta = Math.min(MAX_DELTA, (time - previousTime) / 1000);
+    previousTime = time;
 
-  ship.update(delta);
-  particleSystem.update(delta);
-  crosshair.update([planet, ...enemies]);
+    ship.update(delta);
+    particleSystem.update(delta);
+    crosshair.update([planet, ...enemies]);
 
   // Camera follow
-  let direction = CAMERA_DIRECTION.clone();
-  direction.applyQuaternion(ship.quaternion).setLength(CAMERA_DISTANCE);
-  let cameraTargetPosition = ship.position.clone().add(direction);
-  camera.position.lerp(cameraTargetPosition, CAMERA_VELOCITY * delta);
-  let quaternion = (new THREE.Quaternion()).setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
-  quaternion.multiplyQuaternions(ship.quaternion, quaternion);
-  camera.quaternion.slerp(quaternion, CAMERA_VELOCITY * delta);
+    let direction = CAMERA_DIRECTION.clone();
+    direction.applyQuaternion(ship.quaternion).setLength(CAMERA_DISTANCE);
+    let cameraTargetPosition = ship.position.clone().add(direction);
+    camera.position.lerp(cameraTargetPosition, CAMERA_VELOCITY * delta);
+    let quaternion = (new THREE.Quaternion()).setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
+    quaternion.multiplyQuaternions(ship.quaternion, quaternion);
+    camera.quaternion.slerp(quaternion, CAMERA_VELOCITY * delta);
 
   // update spotlight position and direction
-  direction = SPOTLIGHT_VECTOR.clone();
-  direction.applyQuaternion(ship.quaternion);
-  spotlight.position.copy(ship.position);
-  spotlight.target.position.copy(ship.position.clone().add(direction));
-  spotlight.target.updateMatrixWorld();
+    direction = SPOTLIGHT_VECTOR.clone();
+    direction.applyQuaternion(ship.quaternion);
+    spotlight.position.copy(ship.position);
+    spotlight.target.position.copy(ship.position.clone().add(direction));
+    spotlight.target.updateMatrixWorld();
 
   // Enemies
-  for (let enemy of enemies) {
-    enemy.update(delta);
-  }
+    for (let enemy of enemies) {
+      enemy.update(delta);
+    }
 
-  shotController.update(delta);
+    shotController.update(delta);
 
-  renderer.render(scene, camera);
-  renderer.render(hud.scene, hud.camera);
+    renderer.render(scene, camera);
+    renderer.render(hud.scene, hud.camera);
 
   //Update debugging text
-  if (DEBUG) {
-    text.innerHTML = ['x', 'y', 'z'].map(x => x + ': ' + ship.position[x]).join('<br/>');
-    fps = fps * 9.0 / 10.0;
-    fps += (1.0 / (Math.max(delta, 0.01) * 10));
-    text.innerHTML += '<br/>fps: ' + fps;
+    if (DEBUG) {
+      text.innerHTML = ['x', 'y', 'z'].map(x => x + ': ' + ship.position[x]).join('<br/>');
+      fps = fps * 9.0 / 10.0;
+      fps += (1.0 / (Math.max(delta, 0.01) * 10));
+      text.innerHTML += '<br/>fps: ' + fps;
+    }
+
+    requestAnimationFrame(render);
   }
 
-  requestAnimationFrame(render);
-}
-
-loadPromise.then(() => {
-  initEnemies();
   previousTime = new Date().getTime();
   render();
-});
+}
+
+loader.manager.onLoad = () => {
+  initGame();
+};
+loader.load();
