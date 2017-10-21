a1fd1e8880dc55ff74e9d37336754cad26e7ff4b
N1cc3 <niclaslindgren91@hotmail.com>
2017-10-21 06:38:55 +0300

    Fighter AI now has larger safety distance and shoots only when target in front.

--- src/js/FighterAI.js
+++ src/js/FighterAI.js
@@ -2,7 +2,9 @@ import * as THREE from 'three';
 
 const CLOSE_DISTANCE = 50;
 const FAR_DISTANCE = 200;
-const COLLISION_CHECK_DISTANCE = 20;
+const COLLISION_CHECK_DISTANCE = 30;
+const SAFETY_DISTANCE = 10;
+const SHOOT_ANGLE = 0.05;
 
 // Object pool
 const VECTOR3_A = new THREE.Vector3();
@@ -16,7 +18,7 @@ class FighterAI {
   }
 
   update(ship, delta) {
-    let hitObject = ship.checkCollision(ship.quaternion, COLLISION_CHECK_DISTANCE, ship.hitRadius);
+    let hitObject = ship.checkCollision(ship.quaternion, COLLISION_CHECK_DISTANCE, SAFETY_DISTANCE);
     if (hitObject) {
       let away = VECTOR3_A.subVectors(ship.position, hitObject.position);
       away.add(ship.position);
@@ -25,7 +27,10 @@ class FighterAI {
       if (ship.AIattacking) {
         let aimTarget = this.getAimTarget(ship.position, ship.AItarget.position, ship.AItarget.getVelocityVec(), ship.gun.muzzleVelocity);
         ship.turnTowards(aimTarget, delta);
-        ship.shoot();
+
+        let facing = VECTOR3_A.set(0, 0, 1).applyQuaternion(ship.quaternion);
+        let angleToTarget = facing.angleTo(VECTOR3_B.subVectors(aimTarget, ship.position));
+        if (angleToTarget < SHOOT_ANGLE) ship.shoot();
         if (ship.position.distanceTo(ship.AItarget.position) < CLOSE_DISTANCE) {
           ship.AIattacking = false;
         }
