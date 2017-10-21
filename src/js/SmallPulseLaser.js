0c2b8baf5b49bf87c4d0597ec3f22ec5b6f45571
N1cc3 <niclaslindgren91@hotmail.com>
2017-10-21 06:36:02 +0300

    Fixed shooting on spawn bug.

--- src/js/SmallPulseLaser.js
+++ src/js/SmallPulseLaser.js
@@ -5,7 +5,7 @@ class SmallPulseLaser extends THREE.Object3D {
 
   constructor() {
     super();
-    this.reload = 0.0;
+    this.reload = 1.0;
     this.reloadTime = 0.5;
     this.muzzleVelocity = 300;
     this.shotLifetime = 5.0;
