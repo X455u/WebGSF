c39b91949ccece6f0c2b4d1f046796ace283d932
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-10-09 01:05:19 +0300

    Updated THREE to 0.97. Added three-full as dep. Lensflare used from three-full.

--- src/js/Sun.js
+++ src/js/Sun.js
@@ -1,20 +1,21 @@
 import * as THREE from 'three';
 import {LOADER} from './GSFLoader';
 import {SCENE} from './Game';
+import {Lensflare, LensflareElement} from 'three-full';
 
-function lensFlareUpdateCallback(object) {
-  let f, fl = object.lensFlares.length;
+function LensflareUpdateCallback(object) {
+  let f, fl = object.Lensflares.length;
   let flare;
   let vecX = -object.positionScreen.x * 2;
   let vecY = -object.positionScreen.y * 2;
   for (f = 0; f < fl; f++) {
-    flare = object.lensFlares[ f ];
+    flare = object.Lensflares[ f ];
     flare.x = object.positionScreen.x + vecX * flare.distance;
     flare.y = object.positionScreen.y + vecY * flare.distance;
     flare.rotation = 0;
   }
-  object.lensFlares[2].y += 0.025;
-  object.lensFlares[3].rotation = object.positionScreen.x * 0.5 + THREE.Math.degToRad(45);
+  object.Lensflares[2].y += 0.025;
+  object.Lensflares[3].rotation = object.positionScreen.x * 0.5 + THREE.Math.degToRad(45);
 }
 
 class Sun extends THREE.DirectionalLight {
@@ -34,16 +35,17 @@ class Sun extends THREE.DirectionalLight {
     let texFlare0 = LOADER.get('texFlare0');
     let texFlare2 = LOADER.get('texFlare2');
     let texFlare3 = LOADER.get('texFlare3');
-    let lensFlare = new THREE.LensFlare(texFlare0, 700, 0.0, THREE.AdditiveBlending, new THREE.Color(0xffffff));
-    lensFlare.add(texFlare2, 512, 0.0, THREE.AdditiveBlending);
-    lensFlare.add(texFlare2, 512, 0.0, THREE.AdditiveBlending);
-    lensFlare.add(texFlare2, 512, 0.0, THREE.AdditiveBlending);
-    lensFlare.add(texFlare3, 60, 0.6, THREE.AdditiveBlending);
-    lensFlare.add(texFlare3, 70, 0.7, THREE.AdditiveBlending);
-    lensFlare.add(texFlare3, 120, 0.9, THREE.AdditiveBlending);
-    lensFlare.add(texFlare3, 70, 1.0, THREE.AdditiveBlending);
-    lensFlare.customUpdateCallback = lensFlareUpdateCallback;
-    this.add(lensFlare);
+    let lensflare = new Lensflare();
+    lensflare.addElement(new LensflareElement(texFlare0, 512, 0.0, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare2, 512, 0.0, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare2, 512, 0.0, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare2, 512, 0.0, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare3, 60, 0.6, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare3, 70, 0.7, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare3, 120, 0.9, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.addElement(new LensflareElement(texFlare3, 70, 1.0, new THREE.Color(0xffffff), THREE.AdditiveBlending));
+    lensflare.customUpdateCallback = LensflareUpdateCallback;
+    this.add(lensflare);
   }
 
 }
