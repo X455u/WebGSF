7e1740efa67d1a04e893bb94962bdb3986195ba4
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-10-10 21:57:37 +0300

    Ambient sound from landing pad.

--- src/js/LandingPad.js
+++ src/js/LandingPad.js
@@ -1,6 +1,6 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
 import GameObject from './GameObject';
-import {GAME} from './Game';
+import {GAME, SOUND_LISTENER} from './Game';
 import {LOADER} from './GSFLoader';
 
 class LandingPad extends GameObject {
@@ -8,8 +8,23 @@ class LandingPad extends GameObject {
     super(LOADER.get('landingPadBufferGeometry'), LOADER.get('landingPadMaterial'));
     GAME.addStatic(this, false);
 
-    // this.castShadow = true;
-    // this.receiveShadow = true;
+    this.sound = new THREE.PositionalAudio(SOUND_LISTENER);
+    this.sound.setBuffer(LOADER.get('low_pulsating_hum'));
+    this.sound.setLoop(true);
+    this.sound.setRefDistance(10);
+    this.sound.setDistanceModel('exponential');
+    this.sound.setRolloffFactor(2);
+    this.add(this.sound);
+    this.sound.play();
+  }
+
+  update(delta) {
+    super.update(delta);
+  }
+
+  destroy() {
+    super.destroy();
+    this.sound.stop();
   }
 
 }
