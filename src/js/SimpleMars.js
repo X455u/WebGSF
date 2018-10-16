ea335698a293f7b2cef312a142f0a02277c6e418
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-10-17 02:46:10 +0300

    Mars no longer uses normal map.

--- src/js/SimpleMars.js
+++ src/js/SimpleMars.js
@@ -5,13 +5,10 @@ import {LOADER} from './GSFLoader';
 class SimpleMars extends GameObject {
   constructor(radius, detail) {
     let geometry = new THREE.IcosahedronGeometry(radius, detail);
-    let normalMap = LOADER.get('marsNormalMap');
     let material = new THREE.MeshPhongMaterial({
-      color: 0xAAAAAA,
+      color: 0x666666,
       shininess: 0,
-      map: LOADER.get('marsTexture'),
-      normalMap: normalMap,
-      normalScale: new THREE.Vector2(0.2, 0.2)
+      map: LOADER.get('marsTexture')
     });
     super(geometry, material);
     this.radius = radius;
