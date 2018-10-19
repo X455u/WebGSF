381964ede050b6bae7b0ecdc2f98fa23bf82b1bb
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-10-19 03:40:14 +0300

    Simplified crosshair code.

--- src/js/Crosshair.js
+++ src/js/Crosshair.js
@@ -2,28 +2,28 @@ import * as THREE from 'three';
 
 import {LOADER} from './GSFLoader';
 import {SCENE} from './Game';
-import {CAMERA} from './GSFCamera';
 
 const FAR = 500;
-const SCALING_FACTOR = 10;
 
-// Object pool
-const VECTOR3_A = new THREE.Vector3();
-
-class Crosshair {
+class Crosshair extends THREE.Sprite {
 
   constructor() {
-    this.source = null;
     let crosshairMaterial = new THREE.SpriteMaterial({
       color: 0x00ff00,
       map: LOADER.get('crosshair'),
+      transparent: true,
+      opacity: 0.5,
       blending: THREE.NormalBlending,
       depthWrite: false,
-      depthTest: false
+      depthTest: false,
+      lights: false,
+      sizeAttenuation: false
     });
-    this.sprite = new THREE.Sprite(crosshairMaterial);
-    this.sprite.renderDepth = 0;
-    SCENE.add(this.sprite);
+    super(crosshairMaterial);
+    this.scale.set(0.1, 0.1, 1);
+    SCENE.add(this);
+
+    this.source = null;
   }
 
   setSourceObject(source) {
@@ -32,16 +32,7 @@ class Crosshair {
 
   update() {
     if (!this.source) return;
-    // let hitObject = this.source.checkCollision(this.source.quaternion, FAR);
-    let hitObject;
-    let distance = FAR;
-    if (hitObject) {
-      distance = VECTOR3_A.subVectors(hitObject.position, this.source.position).length();
-    }
-    this.sprite.position.copy(VECTOR3_A.set(0, 0, -1).applyQuaternion(this.source.quaternion).multiplyScalar(distance).add(this.source.position));
-    let scale = VECTOR3_A.subVectors(this.sprite.position, CAMERA.position).length() / SCALING_FACTOR;
-    this.sprite.scale.x = scale;
-    this.sprite.scale.y = scale;
+    this.position.set(0, 0, -1).applyQuaternion(this.source.quaternion).multiplyScalar(FAR).add(this.source.position);
   }
 
 }
