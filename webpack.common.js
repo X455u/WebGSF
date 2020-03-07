f1c7fe30d4300dee9d611a3a3180e58dbbd1f30b
Niclas Lindgren <niclaslindgren91@hotmail.com>
2020-03-08 00:16:57 +0200

    Add service worker to cache files

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
 /* eslint-env node */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -27,5 +29,67 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html'
+    }),
+    new WebpackPwaManifest({
+      inject: false,
+      fingerprints: false,
+
+      name: 'Galactic Star Fighter',
+      'short_name': 'GSF',
+      'start_url': 'index.html',
+      display: 'fullscreen',
+      orientation: 'landscape',
+      icons: [
+        {
+          src: 'media/touch/touch-icon-57x57.png',
+          sizes: '57x57',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-72x72.png',
+          sizes: '72x72',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-76x76.png',
+          sizes: '76x76',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-114x114.png',
+          sizes: '114x114',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-120x120.png',
+          sizes: '120x120',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-180x180.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: 'media/touch/touch-icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }
+      ]
+    })
+  ]
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   entry: {
@@ -14,10 +13,5 @@ module.exports = merge(common, {
     contentBase: path.join(__dirname, ''),
     compress: true,
     port: 9000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html'
-    })
-  ]
+  }
 });
