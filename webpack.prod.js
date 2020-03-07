049e63adc1ef8915889ef042c79f2ca93fc17d84
Niclas Lindgren <niclaslindgren91@hotmail.com>
2020-03-08 00:16:57 +0200

    Add service worker to cache files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,22 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      exclude: [/\.(?:png|jpg|jpeg|svg|json|mp3)$/],
+      runtimeCaching: [{
+        urlPattern: /\.(?:png|jpg|jpeg|svg|json|mp3)$/,
+        handler: 'CacheFirst',
+
+      }],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ]
 });
