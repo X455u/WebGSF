e168d92bae6ff1695b2702ddfa12fda5421f42a8
Niclas Lindgren <niclaslindgren91@hotmail.com>
2020-03-19 22:32:34 +0200

    Various config fixes

--- deploy-gh-pages.js
+++ deploy-gh-pages.js
@@ -1,5 +1,8 @@
 const ghpages = require('gh-pages');
 
+console.log('Deploying to gh-pages...');
+
 ghpages.publish('build', function(err) {
   if (err) console.error(err);
+  else console.log('DONE!');
 });
