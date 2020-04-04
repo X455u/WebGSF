b59f5d45b430ee031a95c977281d3605f9cf22d9
Niclas Lindgren <niclaslindgren91@hotmail.com>
2020-04-04 19:38:38 +0300

    Add typescript

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(?:js|ts)$/,
         exclude: /node_modules/,
         use: {loader: 'babel-loader'}
       },
