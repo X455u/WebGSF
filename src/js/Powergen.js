989af784373bf3b74ef7693ef2da67297e2109a8
Niclas Lindgren <niclaslindgren91@hotmail.com>
2020-04-04 22:52:36 +0300

    Add powergen explosion

--- src/js/Powergen.js
+++ src/js/Powergen.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import Explosion from './Explosion'
 import { GAME, SOUND_LISTENER } from './Game'
 import GameObject from './GameObject'
 import { LOADER } from './GSFLoader'
@@ -6,6 +7,8 @@ import { LOADER } from './GSFLoader'
 class Powergen extends GameObject {
   constructor() {
     super(LOADER.get('powergenGeometry'), LOADER.get('powergenMaterial'))
+
+    this.hp = 30
 
     this.sound = new THREE.PositionalAudio(SOUND_LISTENER)
     this.sound.setBuffer(LOADER.get('low_pulsating_hum'))
@@ -22,6 +25,21 @@ class Powergen extends GameObject {
     this.isStatic = true
 
     this.collisionHulls = [new THREE.Geometry().fromBufferGeometry(this.geometry).vertices]
+  }
+
+  dealDamage(damage) {
+    this.hp -= damage
+    if (this.hp <= 0) {
+      const explosion = new Explosion()
+      explosion.position
+        .copy(this.position)
+        .addScaledVector(new THREE.Vector3().copy(this.up).applyQuaternion(this.quaternion), 2.5)
+      GAME.addObject(explosion)
+      this.destroy()
+      this.dispatchEvent({
+        type: 'onDeath',
+      })
+    }
   }
 
   destroy() {
